5708395876ca43b18470766bae17b6ad85affdda
Axel Schumacher <axel@squareup.com>
2023-05-07 02:50:27 -0700

    min functional

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,23 @@ use std::{error::Error, fs};
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result: Vec<&'a str> = vec![];
+    let mut results = Vec::new();
+
     for line in contents.lines() {
         if line.contains(query) {
-            result.push(line);
+            results.push(line);
         }
     }
 
-    result
+    results
 }
 
 pub struct Config {
