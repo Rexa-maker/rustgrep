761e260dc90a05a8060bbd9012a9557ab4a5f2ed
Axel Schumacher <axel@squareup.com>
2023-05-10 08:16:44 -0700

    using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::{env, error::Error, fs};
 
+static PROGRAM_NAME: &str = "rustgrep";
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
@@ -17,15 +19,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -41,6 +38,20 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     results
 }
 
+pub fn usage() {
+    eprintln!(
+        "\
+Help:
+Will search a regular expression in a file, printing the matching lines in the standard output.
+
+usage:
+{PROGRAM_NAME} query file_path
+    query: a regular expression to search
+    file_path: the file in which to search
+"
+    );
+}
+
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -48,13 +59,24 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => {
+                usage();
+                return Err("Didn't get a query string");
+            }
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => {
+                usage();
+                return Err("Didn't get a file path");
+            }
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
